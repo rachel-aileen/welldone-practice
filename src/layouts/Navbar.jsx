fa093175435707f7c12fe714a9cece65bacf946b
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-02-01 13:38:24 -0800

    welldone navbar progress

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-
-
+import React, { useState } from 'react';
 
 export default function Navbar() {
+    // State to manage menu visibility
+    const [isMenuVisible, setIsMenuVisible] = useState(false);
+
+    // Function to toggle menu visibility
+    const toggleMenu = () => setIsMenuVisible(!isMenuVisible);
+
     return (
         <div className='navbar'>
-            <a><img src="https://firebasestorage.googleapis.com/v0/b/bloomboxx-16513.appspot.com/o/welldoneLogo.png?alt=media&token=7dfa7208-77ad-4468-a1a0-7238d2bddc4e" className='logo' alt="welldoneLogo" /></a>
+            <a href="/"><img src="https://firebasestorage.googleapis.com/v0/b/bloomboxx-16513.appspot.com/o/welldoneLogo.png?alt=media&token=7dfa7208-77ad-4468-a1a0-7238d2bddc4e" className='logo' alt="welldoneLogo" /></a>
             <div className='navigation'>
-                <ul>
-
+                {/* Toggling visibility based on state */}
+                <ul className={`menu ${isMenuVisible ? 'show' : ''}`}>
+                    <div className="close-btn" onClick={() => setIsMenuVisible(false)}></div>
+                    <li className="menu-item"><a href="index.html">Home</a></li>
+                    <li className="menu-item"><a href="about.html">About</a></li>
+                    <li className="menu-item"><a href="services.html">Services </a></li>
+                    <li className="menu-item"><a href="fees.html">Fees</a></li>
+                    <li className="menu-item"><a href="forms.html" className="active">Forms</a></li>
+                    <li className="menu-item"><a href="contact.html">Contact</a></li>
                 </ul>
+                <div className="menu-btn" onClick={toggleMenu}></div>
             </div>
-
         </div>
-    )
+    );
 }
+
