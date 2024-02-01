ef7d04b6464aad5d84111d219a16c3cc8d0764f1
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-02-01 13:18:36 -0800

    welldone logo (desktop)

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+
+export default function Navbar() {
+    return (
+        <div className='navbar'>
+            <a><img src="https://firebasestorage.googleapis.com/v0/b/bloomboxx-16513.appspot.com/o/welldoneLogo.png?alt=media&token=7dfa7208-77ad-4468-a1a0-7238d2bddc4e" className='logo' alt="welldoneLogo" /></a>
+            <div className='navigation'>
+                <ul>
+
+                </ul>
+            </div>
+
+        </div>
+    )
+}
