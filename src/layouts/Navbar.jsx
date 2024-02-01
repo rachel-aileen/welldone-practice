34181401518fbfff835b3ac0994ece126f899f64
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-02-01 15:13:10 -0800

    navbar desktop (logo and menu icon)

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,3 +1,5 @@
+import { DragHandle, Menu } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
 import React, { useState } from 'react';
 
 export default function Navbar() {
@@ -9,7 +11,9 @@ export default function Navbar() {
 
     return (
         <div className='navbar'>
-            <a href="/"><img src="https://firebasestorage.googleapis.com/v0/b/bloomboxx-16513.appspot.com/o/welldoneLogo.png?alt=media&token=7dfa7208-77ad-4468-a1a0-7238d2bddc4e" className='logo' alt="welldoneLogo" /></a>
+            <a href="/">
+                <h1 className='logo'>Welldone</h1>
+            </a>
             <div className='navigation'>
                 {/* Toggling visibility based on state */}
                 <ul className={`menu ${isMenuVisible ? 'show' : ''}`}>
@@ -21,7 +25,16 @@ export default function Navbar() {
                     <li className="menu-item"><a href="forms.html" className="active">Forms</a></li>
                     <li className="menu-item"><a href="contact.html">Contact</a></li>
                 </ul>
-                <img src="https://firebasestorage.googleapis.com/v0/b/bloomboxx-16513.appspot.com/o/menuBtn.png?alt=media&token=bf0d09c3-2e80-4d6d-af77-37ce95c7e144" className="menu-btn" onClick={toggleMenu} alt="Menu" />            </div>
+
+
+                <IconButton onClick={toggleMenu} className='menuBtn'>
+                    <Menu className='menuIcon' />
+                </IconButton>
+
+
+
+                {/* <img src="https://firebasestorage.googleapis.com/v0/b/bloomboxx-16513.appspot.com/o/menuBtn.png?alt=media&token=bf0d09c3-2e80-4d6d-af77-37ce95c7e144" className="menu-btn" onClick={toggleMenu} alt="Menu" /> */}
+            </div>
         </div>
     );
 }
