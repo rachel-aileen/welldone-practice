06f26c3a14dda188469d541593644622f10a6fbf
rachel-aileen <137586206+rachel-aileen@users.noreply.github.com>
2024-02-03 11:04:46 -0800

    welldone navbar example

--- src/layouts/Navbar.jsx
+++ src/layouts/Navbar.jsx
@@ -1,10 +1,12 @@
-import { DragHandle, Menu } from '@mui/icons-material';
+import { Close, Menu } from '@mui/icons-material';
 import { IconButton } from '@mui/material';
 import React, { useState } from 'react';
 
 export default function Navbar() {
     // State to manage menu visibility
+
     const [isMenuVisible, setIsMenuVisible] = useState(false);
+
 
     // Function to toggle menu visibility
     const toggleMenu = () => setIsMenuVisible(!isMenuVisible);
@@ -16,8 +18,8 @@ export default function Navbar() {
             </a>
             <div className='navigation'>
                 {/* Toggling visibility based on state */}
-                <ul className={`menu ${isMenuVisible ? 'show' : ''}`}>
-                    <div className="close-btn" onClick={() => setIsMenuVisible(false)}></div>
+                <ul className={`menu ${isMenuVisible ? 'active' : 'hidden'}`}>
+                    {/* <div className="close-btn" onClick={() => setIsMenuVisible(false)}></div> */}
                     <li className="menu-item"><a href="index.html">Home</a></li>
                     <li className="menu-item"><a href="about.html">About</a></li>
                     <li className="menu-item"><a href="services.html">Services </a></li>
@@ -26,14 +28,17 @@ export default function Navbar() {
                     <li className="menu-item"><a href="contact.html">Contact</a></li>
                 </ul>
 
-
                 <IconButton onClick={toggleMenu} className='menuBtn'>
-                    <Menu className='menuIcon' />
+                    {!isMenuVisible &&
+                        <Menu className='menuIcon' />
+                    }
+                    {isMenuVisible &&
+                        <Close className='menuIcon' />
+                    }
                 </IconButton>
 
 
 
-                {/* <img src="https://firebasestorage.googleapis.com/v0/b/bloomboxx-16513.appspot.com/o/menuBtn.png?alt=media&token=bf0d09c3-2e80-4d6d-af77-37ce95c7e144" className="menu-btn" onClick={toggleMenu} alt="Menu" /> */}
             </div>
         </div>
     );
